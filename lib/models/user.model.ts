beac17ac5c9f7fe623ca78c84542194a5ed3fcd7
TusharMaithani-2002 <202051194@iiitvadodara.ac.in>
2023-08-20 12:37:57 +0530

    added user model

---
+++ lib/models/user.model.ts
@@ -0,0 +1,31 @@
+import mongoose from '@/lib/mongodb';
+
+const userSchema = new mongoose.Schema({
+    id:{type:String,required:true},
+    username:{type:String,required:true},
+    name:{type:String,requierd:true},
+    image:String,
+    bio:String,
+    threads : [
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Thread'
+        }
+    ],
+
+    onboarding: {
+        type:Boolean,
+        default:false,
+    },
+
+    communities : [
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:'Commuity'
+        }
+    ]
+});
+
+const User = mongoose.models.User || mongoose.model('User',userSchema);
+
+export default User;
