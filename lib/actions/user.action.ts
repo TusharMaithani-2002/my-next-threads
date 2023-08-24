3eea0867cdca60f5738c139d9a4bf06e5c75a0a0
TusharMaithani-2002 <202051194@iiitvadodara.ac.in>
2023-08-24 09:35:08 +0530

    added search page and fetch user action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -4,6 +4,7 @@ import { revalidatePath } from "next/cache";
 import Thread from "../models/thread.model";
 import User from "../models/user.model";
 import { connectToDB } from "../mongodb";
+import {SortOrder,FilterQuery} from 'mongoose';
 
 interface Params {
     userId:string,
@@ -93,5 +94,58 @@ export async function fetchUserPosts(userId:string) {
     } catch(error:any) {
 
         throw new Error(`Failed to fetch posts ${error.message}`);
+    }
+}
+
+export async function fetchUsers({
+    userId,
+    searchString="",
+    pageNumber=1,
+    pageSize=20,
+    sortBy="desc"
+}:{
+    userId:string,
+    searchString? : string,
+    pageNumber?: number,
+    pageSize?:number,
+    sortBy?: SortOrder
+}) {
+
+    connectToDB();
+
+    try{
+
+        const skipAmount = (pageNumber-1) * pageSize;
+
+        const regex = new RegExp(searchString,"i");
+
+        const query : FilterQuery<typeof User> = {
+            id: {$ne:userId}
+        }
+
+        if(searchString.trim() !== '') {
+            query.$or = [
+                {username: {$regex:regex}},
+                {name:{$regex:regex}}
+            ]
+        }
+
+        const sortOptions = {createdAt:sortBy};
+
+        const usersQuery = User.find(query)
+        .sort(sortOptions)
+        .skip(skipAmount)
+        .limit(pageSize)
+
+        const totalUsersCount = await User.countDocuments(query);
+
+        const users = await usersQuery.exec();
+
+        const isNext = totalUsersCount > skipAmount + users.length;
+
+        return {users,isNext};
+
+    } catch(error:any) {
+        throw new Error(`Failed to search users ${error.message}`);
     }
 }
